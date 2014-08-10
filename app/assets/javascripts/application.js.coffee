029bf883d7e934bd04d31878ba349f1929ccb990
Henry <henryhyn@163.com>
2014-08-10 18:09:18 +0800

    集成 CKEditor

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -2,6 +2,7 @@
 #= require jquery_ujs
 #= require turbolinks
 #= require jquery.turbolinks
+#= require ckeditor
 
 $ ->
   $(".button-unstop").click( ->
--- app/controllers/frontend/posts_controller.rb
+++ app/controllers/frontend/posts_controller.rb
@@ -1,12 +1,24 @@
 class Frontend::PostsController < ApplicationController
+  before_filter :find_post, only: [:destroy]
+
   def create
     Post.create(post_params)
     redirect_to frontend_topic_path(post_params[:topic_id])
+  end
+
+  def destroy
+    topic_id = @post.topic_id
+    @post.destroy
+    redirect_to frontend_topic_path(topic_id)
   end
 
   private
 
   def post_params
     params.require(:post).permit(:topic_id, :message)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
